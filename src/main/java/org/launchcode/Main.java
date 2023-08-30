3f60915aa2d33f00bfe30651f3aa0e1b25199cc4
bahbond <bondopicks@gmail.com>
2023-08-29 20:00:38 -0500

    Bonus

--- src/main/java/org/launchcode/Main.java
+++ src/main/java/org/launchcode/Main.java
@@ -7,11 +7,14 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-String input = "If the product of two terms is zero then common sense says at least one of the two terms" +
+/*String input = "If the product of two terms is zero then common sense says at least one of the two terms" +
         " has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics " +
         "into a form that can be factored allowing that side of the equation to equal zero. Once you've done that," +
-        " it's pretty straightforward from there.";
-       char[] charactersInString = input.toCharArray();
+        " it's pretty straightforward from there.";*/
+        Scanner input = new Scanner(System.in);
+        System.out.println("Give me a word to count!");
+        String name = input.nextLine().toLowerCase();
+       char[] charactersInString = name.toCharArray();
        HashMap <Character, Integer> characterCounter = new HashMap<>();
        int counter = 1;
 
